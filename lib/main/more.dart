8675a390ec5822297c5ee847cb55588ad47221f1
Jake <jake.ppjk@gmail.com>
2022-02-23 23:31:58 +0900

    more, setting도 elevation, Listview넣음

--- lib/main/more.dart
+++ lib/main/more.dart
@@ -4,13 +4,42 @@ import 'package:samsung_club/login/login.dart';
 import 'package:samsung_club/main/more/setting.dart';
 import '../cst_bottom_nav.dart' as cst_bottom_nav;
 
-class More extends StatelessWidget {
+class More extends StatefulWidget {
   const More({Key? key}) : super(key: key);
 
   @override
+  State<More> createState() => _MoreState();
+}
+
+class _MoreState extends State<More> {
+  ScrollController scroll = ScrollController();
+  double elevation = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      setState(() {
+        if(scroll.position.pixels > 0){
+          elevation = 5.0;
+        } else{
+          elevation = 0.0;
+        }
+      });
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView.builder(
+          itemCount: 300,
+          controller: scroll,
+          itemBuilder: (c,i){
+            return Text('asd');
+          }
+      ),
       appBar: AppBar(
+        elevation: elevation,
         title: Text(cst_bottom_nav.bottomNav[3]['title']),
         actions: [
           Row(
--- lib/main/more/setting.dart
+++ lib/main/more/setting.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   const Setting({Key? key}) : super(key: key);
+
+  @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  ScrollController scroll = ScrollController();
+  double elevation = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      setState(() {
+        if(scroll.position.pixels > 0){
+          elevation = 5.0;
+        } else{
+          elevation = 0.0;
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView.builder(
+          itemCount: 300,
+          controller: scroll,
+          itemBuilder: (c,i){
+            return Text('asd');
+          }
+      ),
       appBar: AppBar(
+        elevation: elevation,
         title: Text('환경설정'),
       ),
     );
